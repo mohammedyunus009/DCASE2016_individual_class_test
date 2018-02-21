57b36be66ce9da12183127721e5b56faee80f5b2
farooqbhai <mohammedyunusfarooq@gmail.com>
2018-02-21 19:24:18 +0530

    "first commit"

---
+++ test_config.py
@@ -0,0 +1,34 @@
+
+# development config
+
+dev_csv_fd = "TUT-acoustic-scenes-2016-development/evaluation_setup"
+
+dev_tr_csv = [dev_csv_fd+"/fold1_train.txt", dev_csv_fd+"/fold2_train.txt", 
+               dev_csv_fd+"/fold3_train.txt", dev_csv_fd+"/fold4_train.txt"]
+
+dev_te_csv = [dev_csv_fd+"/fold1_evaluate.txt", dev_csv_fd+"/fold2_evaluate.txt", 
+               dev_csv_fd+"/fold3_evaluate.txt", dev_csv_fd+"/fold4_evaluate.txt"]
+dev_meta_csv = "TUT-acoustic-scenes-2016-development/meta.txt"
+
+
+# evaluation config
+eva_meta_csv = "TUT-acoustic-scenes-2016-evaluation/meta.txt"
+
+
+#BACKUP FILES FDS#####################
+
+# development config
+
+bdev_csv_fd = "TUT-acoustic-scenes-2016-development/evaluation_setup"
+
+bdev_tr_csv = [dev_csv_fd+"/fold1_train.backup.txt", dev_csv_fd+"/fold2_train.backup.txt", 
+               dev_csv_fd+"/fold3_train.backup.txt", dev_csv_fd+"/fold4_train.backup.txt"]
+
+bdev_te_csv = [dev_csv_fd+"/fold1_evaluate.backup.txt", dev_csv_fd+"/fold2_evaluate.backup.txt", 
+               dev_csv_fd+"/fold3_evaluate.backup.txt", dev_csv_fd+"/fold4_evaluate.backup.txt"]
+bdev_meta_csv = "TUT-acoustic-scenes-2016-development/meta.backup.txt"
+
+
+# evaluation config
+beva_meta_csv = "TUT-acoustic-scenes-2016-evaluation/meta.backup.txt"
+
